570b7ea297635a227719c9b78aede72a91295a0f
reamaestas <reamaestas@yahoo.com>
2023-09-08 17:24:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+this_string = 'Zero-based indexing!'
+      
+print(this_string[3])
+
+print('Alphabet soup'[5])
+
+
+# ---- Try It! ----
+
+# Change the index values in lines 3 and 5 to see how they affect the output.
+
+
+
+# Enter 40 into the brackets in line 3. What happens when you use an index value that is larger than the length of the string?
+
+
+# Replace 40 inside the brackets with a negative number, like -1. What happens?
