0bd62e3f99e2941320e75f301dd247695d7dc477
reamaestas <reamaestas@yahoo.com>
2023-09-13 21:17:08 +0000

    completed to 8.4

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
-this_string = 'Zero-based indexing!'
-      
-print(this_string[3])
+#triple quotes allows us to creat strings that cover multiple lines
 
-print('Alphabet soup'[5])
+sentence = '''This string covers
+multiple lines.
+Cool!'''
+print(sentence)
+print("---------------")
+#bracket notation
+print(sentence[2])
+print('sentence'[2])
+print(len(sentence))
+# this thows an error: print(sentence[len(sentence)])
+# do this instead:
+print(sentence[len(sentence)-1])
+print("------USING NEGATIVE INDEX INTEGERS---------")
+#left to right is zero basing indexes
+#right to left the index value starts at -1
+print(sentence[-2])
+print('sentence'[-2])
 
+print("---------------")
 
 # ---- Try It! ----
+this_string = 'Zero-based indexing!'
 
-# Change the index values in lines 3 and 5 to see how they affect the output.
+print(this_string[5])
+ #note how there seems to be nothing printed bc it returns a space 
+print('Alphabet soup'[-5])
+print("-----slicing/substring----------")
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+substring = alphabet[2:5]
+print(substring)
+#remember that the end index is NOT included in the substring
+
+#working with empty start/stop index
+fruit = "cucumber"
+#beginningn to index 3 (not including 3)
+print(fruit[:3])
+#index 3 to last index
+print(fruit[3:])
+print("------saving substrings---------")
+vegetable = "carrot"
+
+print("---------------")
+print("---------------")
+print("---------------")
+print("---------------")
+print("---------------")
 
 
+# it will throw and error if it's out of range
 
-# Enter 40 into the brackets in line 3. What happens when you use an index value that is larger than the length of the string?
-
-
-# Replace 40 inside the brackets with a negative number, like -1. What happens?
